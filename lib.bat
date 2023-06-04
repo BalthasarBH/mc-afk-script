b371c5f6ab3012a3df13d857c4541d7fe30dfd20
BalthasarBH <85037212+BalthasarBH@users.noreply.github.com>
2023-06-04 14:23:37 +0200

    Add files via upload

---
+++ lib.bat
@@ -0,0 +1,9 @@
+python3 pip install time
+python3 pip install random
+python3 pip install keyboard
+python3 pip install sys
+echo  
+echo Joine gerne meinem Discord!
+echo https://discord.gg/4PJysZcUVT
+echo  
+pause
