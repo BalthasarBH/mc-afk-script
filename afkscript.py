2f020a34f276b867b916a27339797ea71bb68561
BalthasarBH <85037212+BalthasarBH@users.noreply.github.com>
2023-06-04 14:18:54 +0200

    afkscript.py 2.0
    Dies ist eine weitere / aktuellere version

--- afkscript.py
+++ afkscript.py
@@ -1,25 +1,56 @@
 from time import sleep
 import random
-from pyautogui import press
+import keyboard
+import sys
+
+global stop
+global warten
+global pause
+stop = False
+# Diese Werte dürfen verändert werden
+warten = 2
+pause = random.randrange(6)
+# - - - - - - - - - - - - - - - - - -
 def afkscript():
     while True:
-        nummer = random.randrange(5)
-        randomzeit = random.randrange(4)
-        sleep(randomzeit)
-        if nummer.__int__() == 1:
-            press('w')
-            break
-        elif nummer.__int__() == 2:
-            press('a')
-            break
-        elif nummer.__int__() == 3:
-            press('s')
-            break
-        elif nummer.__int__() == 4:
-            press('d')
-            break
-        elif nummer.__int__() == 5:
-            press('space')
-            break
-print("Script startet in 10 sekunden!!!")
-sleep(12)
+        if stop == False:
+            auswahl = random.randrange(6)
+            sleep(pause)
+            if auswahl.__int__() == 1:  # W
+                keyboard.press('w')
+                sleep(warten)
+                keyboard.press('s')
+
+            elif auswahl.__int__() == 2:  # A
+                keyboard.press('a')
+                sleep(warten)
+                keyboard.press('d')
+
+            elif auswahl.__int__() == 3:  # S
+                keyboard.press('s')
+                sleep(warten)
+                keyboard.press('w')
+
+            elif auswahl.__int__() == 4:  # D
+                keyboard.press('d')
+                sleep(warten)
+                keyboard.press('a')
+
+            elif auswahl.__int__() == 5:  # SPACE
+                keyboard.press('space')
+        elif stop == True:
+            print("Danke das du mein Script benutzt :D")
+            print("Komm auf mein Discord ;D")
+            print("https://discord.gg/4PJysZcUVT")
+              # Stattdessen von exit() zu sys.exit() wechseln
+
+def stop_script():
+    global stop
+    stop = True
+
+print("Drücke ENTER zum starten")
+print("Um das Script zu stoppen, drücke Q")
+keyboard.add_hotkey('q', stop_script)  # Füge die Funktion stop_script() hinzu
+keyboard.wait('enter')
+print("Los geht's!")
+afkscript()
